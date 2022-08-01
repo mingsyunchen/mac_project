ee6e934bbe68b12fb8f39f97becf4b1a6b90f998
syun <106572020+mingsyunchen@users.noreply.github.com>
2022-08-01 16:51:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-app.listen(8765);
-
+// app.listen(8765);
+const PORT = process.env.PORT || 8765;
+app.listen(PORT,console.log(`Server started on port ${PORT}`));
 var emprouter = require('./emprouter');
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
